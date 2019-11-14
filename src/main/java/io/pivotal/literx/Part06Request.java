11db8325235b941c4ccea12868f58b7f169336f9
Przemyslaw Puczka <p.puczka@gmail.com>
2019-11-14 21:32:41 +0100

    reactor train

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -3,6 +3,7 @@ package io.pivotal.literx;
 import io.pivotal.literx.domain.User;
 import io.pivotal.literx.repository.ReactiveRepository;
 import io.pivotal.literx.repository.ReactiveUserRepository;
+import org.w3c.dom.ls.LSOutput;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -19,28 +20,38 @@ public class Part06Request {
 
 	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
 	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
+
+		return StepVerifier.create(flux).expectNextCount(4).expectComplete();
 	}
 
 //========================================================================================
 
 	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE.
 	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
+
+		return StepVerifier.create(flux).expectNext(User.SKYLER).expectNext(User.JESSE).thenCancel();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
 	Flux<User> fluxWithLog() {
-		return null;
+
+		return Flux.from(repository.findAll())
+				.log();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
 	Flux<User> fluxWithDoOnPrintln() {
-		return null;
+
+		return Flux
+				.from(repository.findAll())
+				.doOnSubscribe(s -> System.out.println("Starring"))
+				.doOnNext(next -> System.out.println("firstName lastName"))
+				.doOnComplete(() -> System.out.println("Complete"));
+
 	}
 
 }
